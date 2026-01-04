ce771f70bc2e7345ae363899c285ab93c4eec0ac
nguyenlequananh <lepodz2503@gmail.com>
2026-01-05 00:30:01 +0700

    Cap nhat toan bo du an tu root

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ import UserRouter from './router/UserRouter.js';
 
 app.use(express.json());
 app.use('/api/users', UserRouter);
-x``
 app.listen(3000, () => {
     console.log('Server chạy tại http://localhost:3000');
 });
 // https://github.com/nguyenlequananh/bo_chat_server
+//git add .
+// git commit -m "Cap nhat toan bo du an tu root"
+// git push origin main
