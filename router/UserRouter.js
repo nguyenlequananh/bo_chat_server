2b26d3e769ba1ef5b06fdb19d39099c2324762cf
nguyenlequananh <lepodz2503@gmail.com>
2026-01-07 16:20:35 +0700

    Cap nhat router de lay tin nhan theo conversation ID

--- router/UserRouter.js
+++ router/UserRouter.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import UserController from '../controller/UserController.js';
+import ConversationController from '../controller/ConversationController.js';
 const router = express.Router();
 router.post('/login', UserController.login);
+router.get('/:userId/conversations', ConversationController.getConversationsByUserId);
 export default router;
