f34a0b34d9b2336833bed11e05d68b9cf34edf02
nguyenlequananh <lepodz2503@gmail.com>
2026-01-07 16:23:32 +0700

    Cap nhat router de lay tin nhan theo conversation ID

--- router/ConversationRouter.js
+++ router/ConversationRouter.js
@@ -2,6 +2,6 @@ import express from 'express';
 import ConversationController from '../controller/ConversationController.js';
 import MessageController from '../controller/MessageController.js';
 const router = express.Router();
-router.get('/conversations/:userId', ConversationController.getConversationsByUserId);
-router.get('/conversation/:conversationId/messages', MessageController.getMessagesByConversationId);
+router.get('/:conversationId/messages', MessageController.getMessagesByConversationId ); // Lấy tin nhắn theo conversation ID
+
 export default router;
